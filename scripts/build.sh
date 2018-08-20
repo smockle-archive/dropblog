26655cf5fe9510e8219b0f70c622725b4a508658
Clay Miller <clay@smockle.com>
2018-08-20 01:12:09 -0700

    Add rest of AWS resources and build scripts to CI

---
+++ scripts/build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# Abort if any command (incl. in pipeline) exits with error
+set -e
+set -o pipefail
+
+# List of build directories
+declare -a build_directories=(
+  "dropbox-to-sns"
+  "sns-to-s3"
+  "create-s3-to-github"
+  "delete-s3-to-github"
+)
+
+for index in ${!build_directories[*]}; do
+  # Short-circuit if build directory does not exist
+  # Short-circuit if build directory does not contain a TypeScript config file
+  if [ ! -d "${build_directories[$index]}" ] || [ ! -f "${build_directories[$index]}/tsconfig.json" ]; then
+    continue
+  fi
+
+  # Clean-up previously-compiled files
+  zipfile="$(basename "${build_directories[$index]}").zip"
+  set +e
+  rm "${build_directories[$index]}/index.js" 2>/dev/null
+  rm "${build_directories[$index]}/${zipfile}" 2>/dev/null
+  set -e
+
+  # Install dependencies
+  npm --prefix "${build_directories[$index]}" install "${build_directories[$index]}"
+
+  # Compile TypeScript using config in 'tsconfig.json'
+  "${build_directories[$index]}/node_modules/.bin/tsc" -p "${build_directories[$index]}/tsconfig.json"
+
+  # Package depedencies and compiled code for AWS Lambda
+  zip -r "${build_directories[$index]}/${zipfile}" "${build_directories[$index]}"
+  unset zipfile
+done
+
+unset build_directories
---
+++ scripts/deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Abort if any command (incl. in pipeline) exits with error
+set -e
+set -o pipefail
+
+# Upload 'swagger.json' to an S3 bucket
+# - https://stackoverflow.com/a/42779465
+# - https://github.com/awslabs/serverless-application-model/issues/345#issuecomment-408121503
+# - https://github.com/awslabs/serverless-application-model/issues/305#issuecomment-375297669
+aws s3 cp ./swagger.json "s3://$AWS_S3_BUCKET_NAME/"
+
+# Create CloudFormation template
+sam package --template-file template.json \
+            --s3-bucket "$AWS_S3_BUCKET_NAME" \
+            --output-template-file packaged.yaml
+
+# Create (or update) CloudFormation stack
+sam deploy --template-file packaged.yaml \
+           --capabilities CAPABILITY_IAM \
+           --parameter-overrides \
+               AwsS3BucketName="$AWS_S3_BUCKET_NAME" \
+               DropboxAppSecret="$DROPBOX_APP_SECRET" \
+               DropboxUserId="$DROPBOX_USER_ID" \
+               DropboxUserToken="$DROPBOX_USER_TOKEN" \
+               GitHubToken="$GITHUB_TOKEN" \
+               GitHubUsername="$GITHUB_USERNAME" \
+               GitHubRepo="$GITHUB_REPO" \
+           --stack-name dropblog
